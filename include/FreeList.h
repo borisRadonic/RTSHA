666ca6d0b4611bb66b93455d7b41807811f9b777
Boris <boris@radonic.local.ch>
2023-08-18 12:50:39 +0200

    Simple List

--- include/FreeList.h
+++ include/FreeList.h
@@ -6,9 +6,67 @@
 
 namespace internal
 {
+	template<typename T>
+	class alignas(sizeof(size_t)) FreeLinkedList
+	{
+	private:
+		struct Node
+		{
+			T data;
+			Node* next;
+			explicit Node(size_t address, const T& d, Node* head) : data(d), next(head)
+			{
+				Node* this_object = reinterpret_cast<Node*>(address);
+				*this_object = *this;
+			}
+		};
+		Node* head;
+
+	public:
+
+		explicit FreeLinkedList(rtsha_page* page) : head(NULL), _page(page)
+		{
+		}
+
+		~FreeLinkedList()
+		{
+		}
+
+		rtsha_attr_inline void push(const T& address)
+		{
+			Node newNode(_page->lastFreeBlockAddress, address, head);
+			head = reinterpret_cast<Node*>(_page->lastFreeBlockAddress);
+		}
+
+		rtsha_attr_inline bool is_empty() const
+		{
+			return (head == nullptr);
+		}
+
+		rtsha_attr_inline T pop()
+		{
+			T ret(0U);
+			if (!is_empty() && head != NULL )
+			{
+				Node* temp = head;			
+				ret = temp->data;
+				head = head->next;
+			}			
+			return ret;
+		}
+		
+	private:
+		rtsha_page* _page;									///< The memory page being managed by the free list.
+	};
+
+
 	using namespace std;
 	
+#ifdef USE_STL_LIST
 	using flist = std::forward_list<size_t, InternListAllocator<size_t>>;
+#else
+	using flist = FreeLinkedList<size_t>;
+#endif
 
 	/**
 	* @class FreeList
@@ -43,7 +101,11 @@ namespace internal
 		*/
 		rtsha_attr_inline void push(const size_t address)
 		{
+#ifdef USE_STL_LIST
 			ptrLlist->emplace_front(address);
+#else
+			ptrLlist->push(address);
+#endif
 		}
 
 		/**
@@ -53,6 +115,7 @@ namespace internal
 		*/
 		rtsha_attr_inline size_t pop()
 		{
+#ifdef USE_STL_LIST
 			flist::iterator ptr = ptrLlist->begin();
 			if (ptr != ptrLlist->end())
 			{
@@ -61,26 +124,21 @@ namespace internal
 				return address;
 			}
 			return 0U;
+#else
+			return ptrLlist->pop();
+#endif
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	private:
 		rtsha_page* _page;									///< The memory page being managed by the free list.
+		
+#ifdef USE_STL_LIST
+		
 		InternListAllocator<std::size_t>* _lallocator;		///< Internal list allocator for storin 'list' memory blocks in 'free blocks' space
+#endif
 		flist* ptrLlist;									///< Pointer to the list structure used internally.
 
+#ifdef USE_STL_LIST
 	private:
 		/**
 		* @brief Reserved for storage on the stack.
@@ -91,7 +149,9 @@ namespace internal
 		PREALLOC_MEMORY<size_t> _internal_list_storage = 0U;
 		
 		/// @brief Reserved storage for an object that will be created using 'placement new' on the stack.
-		PREALLOC_MEMORY <InternListAllocator<size_t>> _storage_allocator = 0U;
+		PREALLOC_MEMORY <InternListAllocator<size_t>> _storage_allocator = 0U;		
+#endif
+		/// @brief Reserved storage for an object that will be created using 'placement new' on the stack.
 		PREALLOC_MEMORY<flist> _storage_list = 0U;
 	};
 }
--- src/FreeList.cpp
+++ src/FreeList.cpp
@@ -5,8 +5,14 @@ namespace internal
 	FreeList::FreeList(rtsha_page* page)
 		:_page(page)
 	{
+
+		
+#ifdef USE_STL_LIST
 		/*create objects on stack using new in place*/
 		_lallocator = new (_storage_allocator.get_ptr()) InternListAllocator<size_t>( page, reinterpret_cast<size_t*>(&_internal_list_storage));
 		ptrLlist = new (_storage_list.get_ptr()) flist(*_lallocator);
+#else
+		ptrLlist = new (_storage_list.get_ptr()) flist(page);
+#endif
 	}
 }
